14062c150dec31e70697a5437b5fd9d30e09bee0
Pascal Engel <ip4ssi@gmail.com>
2022-05-02 13:07:52 +0200

    remove unused aliases

--- zsh.nix
+++ zsh.nix
@@ -4,7 +4,7 @@
   programs.zsh = {
     enable = true;
     autosuggestions.enable = true;
-    # syntaxHighlighting.enable = true;
+    syntaxHighlighting.enable = true;
     ohMyZsh = {
       enable = true;
       theme = "agnoster";
@@ -12,18 +12,11 @@
     };
     shellAliases = {
       # emacs = "emacsclient -c";
-      # ls = "ls -lh --color=auto";
-      please = "sudo";
-      ll = "ls -a";
-      rtv = ''export BROWSER=linkopen; export EDITOR=vim; export PAGER=less;rtv'';
-      vimread = "vim -RNu ~/.vimreadrc";
-      randomYoutube = "mpv (shuf /var/tmp/youtubeVideos)";
+      ll = "ls -la";
       "." ="cd ..";
       ".." = "cd ../..";
       "..." = "cd ../../..";
       "...." = "cd ../../../..";
-      poweroff = "closeAllWindows; poweroff";
-      reboot = "closeAllWindows; reboot";
       e = ''emacsclient -c'';
       nix-shell = ''nix-shell --run zsh'';
     };
