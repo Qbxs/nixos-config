a28986b34bb5444168b147d97906c667b9bd1b07
Qbxs <ip4ssi@gmail.com>
2022-05-04 16:31:53 +0200

    move home config to home.nix

---
+++ home.nix
@@ -0,0 +1,58 @@
+{ config, pkgs, ... }:
+
+let
+  home-manager = builtins.fetchTarball "https://github.com/nix-community/home-manager/archive/master.tar.gz";
+  real-name = "Pascal Engel";
+  email = "ip4ssi@gmail.com";
+in
+{
+  imports = [ (import "${home-manager}/nixos") ];
+
+  home-manager.users.pascal = {
+    /* Here goes your home-manager config, eg home.packages = [ pkgs.foo ]; */
+    nixpkgs.config.allowUnfree = true;
+
+    programs = {
+      git = {
+        enable = true;
+        userName = real-name;
+        userEmail = email;
+        signing = {
+          gpgPath = "/run/current-system/sw/bin/gpg";
+          key = "3FFB5E924B624E438AA13488FDE0094719249572";
+          signByDefault = true;
+        };
+        extraConfig = {
+          color.ui = "auto";
+          commit.gpgsign = true;
+          tag.ForceSignAnnotated = true;
+        };
+      };
+      emacs = {
+        enable = true;
+        extraPackages = epkgs: with epkgs; [
+          zerodark-theme
+          nix-mode
+          nixos-options
+          company-nixos-options
+          haskell-mode
+          flycheck
+          elpy
+          py-autopep8
+          evil
+          yasnippet
+          ess
+        ];
+      };
+      vscode = {
+        enable = true;
+        extensions = with pkgs.vscode-extensions; [
+          yzhang.markdown-all-in-one
+          arcticicestudio.nord-visual-studio-code
+          haskell.haskell
+          bbenoist.nix
+        ];
+      };
+    };
+  };
+}
