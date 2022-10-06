f32f57f590f536bc7de43cf4e5c809fe73439b5f
Qbxs <ip4ssi@gmail.com>
2022-10-06 21:45:36 +0200

    add latex layer

--- doom.d/init.el
+++ doom.d/init.el
@@ -145,7 +145,8 @@
        ;;javascript        ; all(hope(abandon(ye(who(enter(here))))))
        ;;julia             ; a better, faster MATLAB
        ;;kotlin            ; a better, slicker Java(Script)
-       ;;latex             ; writing papers in Emacs has never been so fun
+       (latex +latexmk)    ; writing papers in Emacs has never been so fun
+       latex-preview-pane  : auto-preview latex files
        ;;lean              ; for folks with too much to prove
        ;;ledger            ; be audit you can be
        ;;lua               ; one-based indices? one-based indices
