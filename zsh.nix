363f08a44abe7c4e3f5138b0ba7190a9212e003a
Pascal Engel <pascalengel@posteo.de>
2022-12-02 11:11:37 +0100

    update nixos-config alias

--- zsh.nix
+++ zsh.nix
@@ -18,7 +18,7 @@
       "...." = "cd ../../../..";
       emacs = ''emacsclient -c -a "emacs"'';
       nix-shell = ''nix-shell --run zsh'';
-      nixos-config = ''code /etc/nixos/configuration.nix'';
+      nixos-config = ''code ~/nixos-config/.'';
       mo2 = ''STEAM_COMPAT_CLIENT_INSTALL_PATH=/usr/games/steam STEAM_COMPAT_DATA_PATH=~/.steam/steam/steamapps/compatdata/489830/ steam-run ~/.steam/steam/steamapps/common/Proton\ 7.0/proton run ~/.steam/steam/steamapps/compatdata/489830/pfx/drive_c/Modding/MO2/ModOrganizer.exe'';
     };
     shellInit = ''
