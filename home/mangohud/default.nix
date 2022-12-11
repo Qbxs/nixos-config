5f79c57a4728d8259ce8af3c623fa07e620fbce2
Pascal Engel <pascalengel@posteo.de>
2022-12-11 16:39:49 +0100

    configure mangohud to only display on demand

--- home/mangohud/default.nix
+++ home/mangohud/default.nix
@@ -4,6 +4,13 @@
   programs.mangohud = {
     enable = true;
     enableSessionWide = true;
-    settings.toggle_hud = "Shift_R+F12";
+    settings = {
+      toggle_hud = "Shift_R+F12";
+      vram = true;
+      ram = true;
+      gamemode = true;
+      vkBasalt = true;
+      no_display = true;
+    };
   };
 }
