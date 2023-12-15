973e3731d9efa61f79d5648186317737da635e6f
Pascal Engel <pascalengel@posteo.de>
2023-12-15 18:18:20 +0100

    update to NixOS 23.11

--- flake.nix
+++ flake.nix
@@ -2,11 +2,11 @@
   description = "Flake for nixOS configuration";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
     # for frequent updates, only update this input:
-    nixpkgs-newest.url = "github:NixOS/nixpkgs/nixos-23.05";
+    nixpkgs-newest.url = "github:NixOS/nixpkgs/nixos-23.11";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
-    home-manager.url = "github:nix-community/home-manager/release-23.05";
+    home-manager.url = "github:nix-community/home-manager/release-23.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     nix-doom-emacs.url = "github:nix-community/nix-doom-emacs";
     nix-doom-emacs.inputs.nixpkgs.follows = "nixpkgs";
