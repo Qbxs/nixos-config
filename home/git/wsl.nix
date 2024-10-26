f7b7be3b653da4385c6e0558b69dd2458b70c3b6
Pascal Engel <pascalengel@posteo.de>
2024-10-26 16:12:27 +0200

    add work email to wsl git config

--- home/git/wsl.nix
+++ home/git/wsl.nix
@@ -3,6 +3,7 @@
 {
   imports = [ ./common.nix ];
   programs.git = {
+    userEmail = "engel@itestra.com";
     signing = {
       gpgPath = "/run/current-system/sw/bin/gpg";
       key = "316B3057427BB2B9CDBF8DF95BC89F7EC068CBCF";
